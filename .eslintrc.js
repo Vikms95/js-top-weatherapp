60cc6699e656b2dbae4a61545967e234ac619572
Vikms95 <vms1995@hotmail.com>
2022-03-23 05:10:57 +0100

    Setup project

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'windows'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
+}
