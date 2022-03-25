ca723ca2e5bfaec48ac092fd8113d3432f642635
Vikms <vms1995@hotmail.com>
2022-03-25 13:50:08 +0100

    Import icons from dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,15 @@
 const path = require('path')
 
-
-
 module.exports = {
-
     entry: './src/index.js',
     devtool: 'inline-source-map',
     output: {
-
         filename: 'main.js',
-
         path: path.resolve(__dirname, 'dist'),
-
-    }
+    },
+    module:{
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    } 
 }
 
